1776dd5b137164b553ebcb90e6cdc3761f26e0fd
Renzo Coveñas <renzojcr@hotmail.com>
2026-06-24 13:57:34 -0500

    corrige proxy websocket en nginx

--- frontend/src/services/websocketService.js
+++ frontend/src/services/websocketService.js
@@ -7,15 +7,6 @@ function getWebSocketUrl() {
 
   const protocol = window.location.protocol === "https:" ? "wss" : "ws";
 
-  /*
-   * En producción:
-   * http://57.156.65.62  -> ws://57.156.65.62/ws
-   *
-   * En local con Vite, si no defines VITE_WS_URL,
-   * intentará conectarse al mismo host de Vite.
-   * Para local puedes crear frontend/.env con:
-   * VITE_WS_URL=ws://localhost:8080/ws
-   */
   return `${protocol}://${window.location.host}/ws`;
 }
 
